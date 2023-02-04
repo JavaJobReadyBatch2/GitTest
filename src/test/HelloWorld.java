8aaf27f8fdd9a8ea04729f5cda61d69e7e90172a
Min Thu Kyaw <minthukyaw168@gmail.com>
2023-02-04 17:03:03 +0630

    added shoppingcart

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -9,6 +9,8 @@ public class HelloWorld {
 		System.out.println("Test vi Editor");
 		
 		System.out.println("Inside Developer_001 Branch");
+		
+		System.out.println("to test shopping cart features");
 	}
 
 }
